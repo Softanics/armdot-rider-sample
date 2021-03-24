fe80faeebf3b942bda5705ed0ff7408950b76577
Artem Razin <razin@softanics.com>
2021-03-24 17:10:05 +0300

    Added project files

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a sample for the tutorial:
+https://www.armdot.com/blog/?p=69&preview=true
---
+++ armdot-rider-sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace armdot_rider_sample
+{
+    class Program
+    {
+        [ArmDot.Client.VirtualizeCode]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter password and press ENTER");
+
+            if (CheckPassword(Console.ReadLine()))
+                Console.WriteLine("The password is correct");
+            else
+                Console.WriteLine("The password is not correct");
+        }
+
+        [ArmDot.Client.VirtualizeCode]
+        static bool CheckPassword(string value)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashValue = sha256.ComputeHash(Encoding.UTF8.GetBytes(value));
+                return "mZfua8BSQJP337Kuj4Cpl9dVBL/S6Cn1SioM0xcq2tg=" == Convert.ToBase64String(hashValue);
+            }
+        }
+    }
+}
